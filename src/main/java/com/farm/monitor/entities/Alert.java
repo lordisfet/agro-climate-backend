e9b6e4161021af310d2ecfac18d8c6c2641624df
lordisfet <lordisfet@gmail.com>
2026-06-03 00:14:35 +0300

    Add alert entity

--- src/main/java/com/farm/monitor/entities/Alert.java
+++ src/main/java/com/farm/monitor/entities/Alert.java
@@ -1,10 +1,14 @@
 package com.farm.monitor.entities;
 
+import jakarta.persistence.ForeignKey;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -19,5 +23,11 @@ public class Alert {
     @Column(name = "id", nullable = false, unique = true)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "measurement_id", nullable = true, foreignKey = @ForeignKey(name = "fk_alerts_measurement_id"))
+    private Measurement measurement;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "control_rule_id", nullable = true, foreignKey = @ForeignKey(name = "fk_alerts_control_rule_id"))
+    private ControlRule controlRule;
 }
