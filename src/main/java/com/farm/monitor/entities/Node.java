b157d8fd3ce88f5c249739b8252f681b5edc945a
lordisfet <lordisfet@gmail.com>
2026-06-02 22:33:37 +0300

    Add node entity

--- src/main/java/com/farm/monitor/entities/Node.java
+++ src/main/java/com/farm/monitor/entities/Node.java
@@ -1,7 +1,41 @@
 package com.farm.monitor.entities;
 
-import jakarta.persistence.Entity;
+import com.farm.monitor.enums.Location;
+import com.farm.monitor.enums.Status;
 
+import jakarta.persistence.CheckConstraint;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "nodes")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Node {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, unique = true)
+    private int id;
+
+    @Column(name = "dev_eui", nullable = false, unique = true, check = 
+    @CheckConstraint(name = "dev_eui_check", constraint = "dev_eui ~ '^[0-9A-Fa-f]{16}$'"))
+    private String devEUI;
+
+    @Column(name = "status", nullable = true)
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "location", nullable = true)
+    @Enumerated(EnumType.STRING)
+    private Location location;
 }
