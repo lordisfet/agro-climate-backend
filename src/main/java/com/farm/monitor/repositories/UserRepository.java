ad6d889d139348d1a4f1d43e17c946d96ca6470c
lordisfet <lordisfet@gmail.com>
2026-06-05 14:01:39 +0300

    User initialization

---
+++ src/main/java/com/farm/monitor/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.farm.monitor.repositories;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.farm.monitor.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    
+}
---
+++ src/main/java/com/farm/monitor/services/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.farm.monitor.services;
+
+import com.farm.monitor.entities.User;
+import com.farm.monitor.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPasswordHash())
+                .roles(user.getRole().toString())
+                .disabled(!user.getActive())
+                .build();
+    }
+}
