4b29388efc3299125d40e549f11ad9e2d54a8091
lordisfet <lordisfet@gmail.com>
2026-06-15 05:08:13 +0300

    Fix NodeDTO

--- src/main/java/com/farm/monitor/dto/NodeDTO.java
+++ src/main/java/com/farm/monitor/dto/NodeDTO.java
@@ -6,19 +6,15 @@ import com.farm.monitor.entities.Node;
 import com.farm.monitor.enums.Location;
 import com.farm.monitor.enums.Status;
 
-import jakarta.persistence.CheckConstraint;
-
 public record NodeDTO(
-    Long devEUI,
+    String devEUI,
     Status status,
     Location location,
     Instant lastUpdate
 ) 
 {
-    public NodeDTO() {
-        this(null, null, null, null);
-    }
     public NodeDTO(Node node) {
         this(node.getDevEUI(), node.getStatus(), node.getLocation(), node.getLastUpdate());
     }
 }
+ 
